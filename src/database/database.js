de484bd19ef912873b63a468dd4c83c9a92ff48e
lcfernandez <lcfernandez@cos.ufrj.br>
2022-12-07 23:41:55 -0300

    feat: connect postgres database

---
+++ src/database/database.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import pkg from "pg";
+
+// config
+dotenv.config();
+
+// database connection
+const { Pool } = pkg;
+
+export const connection = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
