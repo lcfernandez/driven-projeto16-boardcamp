017e0e66db400165b89149e2fce14add111ab7ce
lcfernandez <lcfernandez@cos.ufrj.br>
2022-12-08 14:30:54 -0300

    feat: implement get games query string filter

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,8 +1,10 @@
 import { connection } from "../database/database.js";
 
 export async function getGames(req, res) {
+    const { name } = req.query;
+
     try {
-        const games = connection.query("SELECT * FROM games;");
+        const games = connection.query(`SELECT * FROM games ${name && `WHERE name LIKE '%${name}'`};`);
         res.send(games.rows);
     } catch (err) {
         console.error(err);
