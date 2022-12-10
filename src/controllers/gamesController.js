f24327f5fb483c1a7cadd8fccb70c7975a1982ce
lcfernandez <lcfernandez@cos.ufrj.br>
2022-12-09 22:40:56 -0300

    fix: add categoryName in games response

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -2,7 +2,13 @@ import { connection } from "../database/database.js";
 
 export async function getGames(req, res) {
     try {
-        const games = await connection.query("SELECT * FROM games;");
+        const games = await connection.query(
+            `SELECT
+                games.*,
+                categories.name AS "categoryName"
+            FROM games
+            JOIN categories ON games."categoryId" = categories.id;`
+        );
         res.send(games.rows);
     } catch (err) {
         console.error(err);
