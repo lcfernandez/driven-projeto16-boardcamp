5f6d29930463e1a926ccb1efe1ec80a49787e792
lcfernandez <lcfernandez@cos.ufrj.br>
2022-12-10 08:35:21 -0300

    feat: implement get customers query string filter by cpf

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,8 +1,15 @@
 import { connection } from "../database/database.js";
 
 export async function getCustomers(req, res) {
+    const { cpf } = req.query;
+
     try {
-        const customers = await connection.query("SELECT * FROM customers;");
+        const customers = await connection.query(
+            `SELECT * FROM customers
+            ${cpf ? "WHERE cpf LIKE $1||'%'" : ""}
+            ;`,
+            cpf ? [cpf] : ""
+        );
         res.send(customers.rows);
     } catch (err) {
         console.error(err);
