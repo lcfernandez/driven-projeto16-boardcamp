c59528091763dfcd5444979af6dd6adeabe8d603
lcfernandez <lcfernandez@cos.ufrj.br>
2022-12-10 10:35:59 -0300

    fix: prevent JS date conversion

--- src/database/database.js
+++ src/database/database.js
@@ -3,10 +3,11 @@ import pkg from "pg";
 
 // config
 dotenv.config();
+pkg.types.setTypeParser(1082, str => str); // 1082 for date type; prevent JS date conversion
 
 // database connection
 const { Pool } = pkg;
 
 export const connection = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL
 });
