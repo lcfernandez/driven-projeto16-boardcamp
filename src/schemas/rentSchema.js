dd9787d3515e7dc962a6c5f90a284647d7ddce67
lcfernandez <lcfernandez@cos.ufrj.br>
2022-12-12 14:27:17 -0300

    fix: remove strict of deysRented number validation in rent schema

--- src/schemas/rentSchema.js
+++ src/schemas/rentSchema.js
@@ -4,6 +4,6 @@ export const rentSchema = Joi.object(
     {
         customerId: Joi.number().integer().strict().min(1).required(),
         gameId: Joi.number().integer().strict().min(1).required(),
-        daysRented: Joi.number().integer().strict().min(1).required()
+        daysRented: Joi.number().integer().min(1).required()
     }
 );
